6bf5a27da0b6ae1a6b4be8d351e77386fb383dfe
Harsh Garg <harshthegarg@gmail.com>
2025-08-17 19:16:44 +0530

    Toggle transparent bg

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,12 +1,26 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        require("catppuccin").setup({
-            flavour = "mocha",
-            transparent_background = true,
-        })
-        vim.cmd.colorscheme("catppuccin")
-    end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		local bg = true
+		local toggle = function()
+			bg = not bg
+			require("catppuccin").setup({
+				flavour = "mocha",
+
+				transparent_background = bg,
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end
+
+        vim.keymap.set("n", "<leader>bg", toggle, {})
+
+		require("catppuccin").setup({
+			flavour = "mocha",
+
+			transparent_background = true,
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
